1d0713041433cda53ae54b0071f18947411ff075
alexveryk <veryk.av@gmail.com>
2023-04-09 23:18:23 +0300

    Update ContactList.jsx

--- src/components/ContactLst/ContactList.jsx
+++ src/components/ContactLst/ContactList.jsx
@@ -2,25 +2,14 @@ import { List } from './ContactList.styled';
 import { ListItem } from './ContactList.styled';
 import { BtnDelete } from './ContactList.styled';
 import { useDispatch, useSelector } from 'react-redux';
-// import { deleteContact } from "redux/contactsSlice";
-import { getContacts, getFilter } from 'redux/selectors';
+import { selectVisibleName } from 'redux/selectors';
 import { deleteContacts } from 'redux/operations';
 
-const getVisibleName = (contacts, filter) => {
-  const contactNormalize = filter.filter.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(contactNormalize)
-  );
-};
-
 export const ContactLst = () => {
-  const constacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-  console.log('Filter -->', filter);
   const dispatch = useDispatch();
   const handleDelete = id => dispatch(deleteContacts(id));
-  console.log(constacts);
-  const visibleName = getVisibleName(constacts, filter);
+
+  const visibleName = useSelector(selectVisibleName);
 
   return (
     <List>
